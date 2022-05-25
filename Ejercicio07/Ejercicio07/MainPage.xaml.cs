1491895ffc784bbe918bc8acb1e79cf515a55bfb
tcmalware <genarodvo@gmail.com>
2022-05-25 13:24:16 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio07/Ejercicio07/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio07
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushModalAsync(new ViewToViewDemo());
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushModalAsync(new BindingModeDemo());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushModalAsync(new ListViewDemo());
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushModalAsync(new PickerDemo());
+            };
+        }
+    }
+}
